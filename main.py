a14eca8d568a82df5533aa64003320655dfb8be0
Leonid Kolesnichenko <kolesnichenko@gosniias.ru>
2024-07-31 14:57:21 +0300

    [WIP] Add vector_tile method

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
 import os
 
-from pyogrio import read_info
+from pyogrio import read_info, read_dataframe
 import uvicorn
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
 from fastapi import FastAPI
+from mapbox_vector_tile import encode as mvt_encode
 
 from factories import TilerFactory
 
@@ -77,6 +78,37 @@ def driver(uri, security_params=None):
         for k, v in security_params.items():
             os.environ[k] = v
     return read_info(uri)['driver']
+
+@app.route('/vector/tiles/<int:z>/<int:x>/<int:y>.pbf')
+def vector_tile(uri, security_params=None):
+    """Serve vector tiles."""
+    if security_params is not None:
+        security_params = json.loads(security_params)
+        for k, v in security_params.items():
+            os.environ[k] = v
+
+    tile_bounds = tile_to_bounds(x, y, z)
+
+    tile_data = pyogrio.read_dataframe(url, bbox=tile_bounds)
+    tile_geojson = json.loads(pyogrio.to_json(tile_data))
+
+    mvt_data = mvt_encode(tile_geojson, quantize_bounds=None)
+
+    # Serve the MVT data as PBF
+    return send_file(mvt_data, mimetype='application/x-protobuf')
+
+
+def tile_to_bounds(x, y, z):
+    # Convert tile coordinates to web mercator bounds
+    tile_size = 256
+    max_resolution = 156543.03392804097
+    origin_shift = 20037508.342789244
+    res = max_resolution / (2 ** z)
+    x_min = -origin_shift + x * tile_size * res
+    y_max = origin_shift - y * tile_size * res
+    x_max = -origin_shift + (x + 1) * tile_size * res
+    y_min = origin_shift - (y + 1) * tile_size * res
+    return (x_min, y_min, x_max, y_max)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
