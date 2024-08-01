9f02dc05db5892bf9053d6331c9814a0c3c59464
Leonid Kolesnichenko <xperience439@gmail.com>
2024-08-01 08:05:08 +0300

    [WIP] add vector tiles method

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+import pyogrio
+from fastapi.openapi.models import Response
 from pyogrio import read_info, read_dataframe
 import uvicorn
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
@@ -43,6 +45,7 @@ def crs(uri, security_params=None):
             os.environ[k] = v
     return read_info(uri)['crs']
 
+
 @app.get("/vector/geometry_type", description="Bounds of vector file")
 def crs(uri, security_params=None):
     """Health check."""
@@ -51,6 +54,7 @@ def crs(uri, security_params=None):
         for k, v in security_params.items():
             os.environ[k] = v
     return read_info(uri)['geometry_type']
+
 
 @app.get("/vector/fields", description="Bounds of vector file")
 def fields(uri, security_params=None):
@@ -70,6 +74,7 @@ def info(uri, security_params=None):
             os.environ[k] = v
     return str(read_info(uri))
 
+
 @app.get("/vector/driver", description="Bounds of vector file")
 def driver(uri, security_params=None):
     """Health check."""
@@ -79,23 +84,25 @@ def driver(uri, security_params=None):
             os.environ[k] = v
     return read_info(uri)['driver']
 
-@app.route('/vector/tiles/<int:z>/<int:x>/<int:y>.pbf')
-def vector_tile(uri, security_params=None):
+
+@app.get('/vector/tiles')
+def vector_tile(uri, z, x, y, security_params=None):
     """Serve vector tiles."""
     if security_params is not None:
         security_params = json.loads(security_params)
         for k, v in security_params.items():
             os.environ[k] = v
-
+    z,x,y = int(z), int(x), int(y)
     tile_bounds = tile_to_bounds(x, y, z)
 
-    tile_data = pyogrio.read_dataframe(url, bbox=tile_bounds)
-    tile_geojson = json.loads(pyogrio.to_json(tile_data))
+    tile_data = pyogrio.read_dataframe(uri, bbox=tile_bounds)
+    tile_geojson = json.loads(tile_data.to_json())
+    print(tile_geojson)
 
     mvt_data = mvt_encode(tile_geojson, quantize_bounds=None)
 
     # Serve the MVT data as PBF
-    return send_file(mvt_data, mimetype='application/x-protobuf')
+    return Response(content=mvt_data, media_type='application/x-protobuf')
 
 
 def tile_to_bounds(x, y, z):
@@ -109,6 +116,7 @@ def tile_to_bounds(x, y, z):
     x_max = -origin_shift + (x + 1) * tile_size * res
     y_min = origin_shift - (y + 1) * tile_size * res
     return (x_min, y_min, x_max, y_max)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
